010ab05f2130d08f1ba458c1e46fee2528eb329d
Victor <victor.martins.santos05@escola.pr.gov.br>
2025-09-23 11:17:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Função para exibir detalhes de cada time
-function exibirDetalhes(time) {
-    const detalhes = document.getElementById(time);
-    detalhes.innerHTML = `<p>Essas são informações adicionais sobre o ${time.charAt(0).toUpperCase() + time.slice(1)}.</p>`;
-    detalhes.style.display = detalhes.style.display === "none" || detalhes.style.display === "" ? "block" : "none";
+
+// Função para alternar os detalhes dos times
+function toggleDetails(team) {
+    const details = document.getElementById(team);
+    details.style.display = details.style.display === "block" ? "none" : "block";
 }
 
-// Função para abrir as abas de detalhes do pódio
+// Função para abrir as abas interativas de detalhes históricos
 function openTab(evt, tabName) {
     const tabContents = document.querySelectorAll('.tabcontent');
     for (let i = 0; i < tabContents.length; i++) {
